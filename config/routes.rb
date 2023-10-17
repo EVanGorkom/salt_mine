063ecb5a8347f6432858f06c592650ac785490b5
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-17 17:23:04 -0500

    Create the basic routes for the the home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :welcome, only: [:index]
+  resources :register, only: [:index]
 end
